40680b4ac72c41076c694cdda814c56241e7f1fb
Jon <jonsweeney26@gmail.com>
2016-04-03 15:01:35 -0700

    fixed bug by adding "s" to action in index

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
   model(){
     return this.store.findAll('question');
   },
-  action: {
+  actions: {
     save(params) {
       var newQuestion = this.store.createRecord('question', params);
       newQuestion.save();
