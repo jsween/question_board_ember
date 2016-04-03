131cf6ac5ca691e71f65d99cf2a2ef3b806aa8d5
Jon <jonsweeney26@gmail.com>
2016-04-03 14:42:37 -0700

    fixed part of undefined save

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   actions: {
     save(question, params) {
       this.sendAction('save', question, params)
-    }
+    },
     deleteQuestion(question) {
       if(confirm('Are you sure you want to delete this question?')) {
         this.sendAction('delete', question);
