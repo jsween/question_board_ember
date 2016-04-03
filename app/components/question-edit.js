662c2d634f1534ceb18e1045bcf0b16d8b46b7e0
Jon <jonsweeney26@gmail.com>
2016-04-03 14:20:41 -0700

    update edit question form

--- app/components/question-edit.js
+++ app/components/question-edit.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  showEditForm: false,
+  actions: {
+    showEditForm() {
+      this.set('showEditForm', true);
+    },
+    save(question) {
+      this.sendAction('save', question, params);
+    },
+    deleteQuestion(question) {
+      this.sendAction('delete', question);
+    }
+  }
 });
