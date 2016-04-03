e30036573b5a986b7f6f198cbb93144036080f52
Jon <jonsweeney26@gmail.com>
2016-04-03 13:10:12 -0700

    add retrieve questions logic and hbs

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.findAll('question');
+  },
+  action: {
+    //save new question goes here
+  }
 });
