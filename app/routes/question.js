98eafbe70f0af8b4e27b4b4811c2ecfe9d094f8a
Jon <jonsweeney26@gmail.com>
2016-04-03 13:52:37 -0700

    create single question page

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params){
+    return Ember.RSVP.hash({
+      question: this.store.findRecord('question', params.question_id),
+      answers: this.store.findAll('answer')
+    });
+  }
 });
