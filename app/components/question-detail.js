5d920688d161b6bafc6c5071853b317fa8e06493
Jon <jonsweeney26@gmail.com>
2016-04-03 14:03:38 -0700

    create logic for deleting a question

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,5 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  actions: {
+    deleteQuestion(question) {
+      if(confirm('Are you sure you want to delete this question?')) {
+        this.sendAction('delete', question);
+      }
+    }
+  }
 });
