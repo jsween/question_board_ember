4e00602f55436aeacf298d8ec50ad26e33b944b7
Jon <jonsweeney26@gmail.com>
2016-04-03 16:11:11 -0700

    add comments and cleans trouble shooting code

--- app/components/question-edit.js
+++ app/components/question-edit.js
@@ -3,20 +3,19 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   showEditForm: false,
   actions: {
-    showEditForm() {
+    showEditForm() {  //displays form
       this.set('showEditForm', true);
     },
-    save(question) {
+    save(question) {  //catches the changes to the question
       var params = {
         question: this.get('question.question'),
         author: this.get('question.author'),
         notes: this.get('question.notes')
       };
-      this.set('showEditForm', false);
+      this.set('showEditForm', false);//hide form
       this.sendAction('save', question, params);
     },
-    deleteQuestion(question) {
-      console.log('delete from edit question level');
+    deleteQuestion(question) { //sends the selected question to delete up
       this.sendAction('deleteQuestion', question);
     }
   }
