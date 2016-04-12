93f7866d0d6d51d81e54f3a426da15f69f2304fb
Jon <jonsweeney26@gmail.com>
2016-04-12 14:24:20 -0700

    add logic for addToFavorites

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favorites: Ember.inject.service(),
+
   askedBy: Ember.computed('question.author', function() {
     return this.get('question.author') + '!';
-  })
+  }),
+
+  actions: {
+    addToFavorites(question) {
+      this.get('favorites').add(question);
+    }
+  }
 });
