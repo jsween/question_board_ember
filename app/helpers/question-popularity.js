b9b5c987db354b84e18f1b85f1f9fad1f137d5e0
Jon <jonsweeney26@gmail.com>
2016-04-12 14:19:33 -0700

    insert semi colons

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -4,9 +4,9 @@ export function questionPopularity(params) {
   var question = params[0];
   console.log(question);
   if(question.get('answers').get('length') >= 3) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>')
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire"></span>');
   } else if(question.get('answers').get('length') > 0) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-user"></span>')
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-user"></span>');
   }
 }
 
